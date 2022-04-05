f5c3e3b9e1199b4e2cc59efbe5351624ccb8fdf3
tqb310 <baotq310@gmail.com>
2022-04-05 08:03:27 +0700

    Refactor: resources folder

--- routes/authModule/routes.js
+++ routes/authModule/routes.js
@@ -2,10 +2,10 @@ const route = require('express').Router();
 const URL = 'auth';
 
 route.get('/', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 
 module.exports = {
-  URL,
-  route,
+    URL,
+    route,
 };
