458beb8f1171690bbe07db332547708d99b873f8
tqb310 <baotq310@gmail.com>
2022-04-16 10:51:01 +0700

    Config: gulpJs

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, task, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemap = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+
+const path = {
+    scss: {
+        src: ['resources/scss/**/*.scss'],
+        dest: ['public/styles'],
+    },
+    js: {
+        src: ['resources/scripts/**/*.js'],
+        dest: ['public/scripts'],
+    },
+};
+
+task('scss', () => {
+    return src(path.scss.src)
+        .pipe(sourcemap.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(
+            autoprefixer({
+                cascade: false,
+                grid: true,
+            }),
+        )
+        .pipe(cssnano())
+        .pipe(concat('app.min.css'))
+        .pipe(sourcemap.write('.'))
+        .pipe(dest(path.scss.dest));
+});
+
+task('watch', () => {
+    return watch(path.scss.src, series('scss'));
+});
+
+task('default', series('scss', 'watch'));
